b5b7cd9f120d4e1ba975841cc27941f0f3531327
Andrés Navarro S <31898794+AndresNS@users.noreply.github.com>
2021-06-06 21:07:56 -0400

    Refactor UI Prototype

--- 03/js/ui/UserInterface.js
+++ 03/js/ui/UserInterface.js
@@ -15,108 +15,106 @@ function UI() {
   this.characters = [];
 }
 
-/**
- * Methods
- */
+UI.prototype = {
+  initialize: function () {
+    this.playButton.addEventListener("click", () => {
+      if (
+        this.characterSelection1.value === "0" ||
+        this.characterSelection2.value === "0"
+      )
+        return alert("Debes seleccionar ambos personajes para jugar.");
 
-UI.prototype.setControls = function (value) {
-  const controlBtns = [
-    this.attackButton,
-    this.healButton,
-    this.switchButton,
-    this.exitButton,
-  ];
+      this.setControls(false);
+      this.playButton.setAttribute("disabled", true);
+      this.characterSelection1.setAttribute("disabled", true);
+      this.characterSelection2.setAttribute("disabled", true);
+    });
 
-  for (const btn of controlBtns) {
-    if (value) btn.setAttribute("disabled", value);
-    else btn.removeAttribute("disabled");
-  }
-};
+    this.exitButton.addEventListener("click", () => {
+      this.setControls(true);
+      this.playButton.removeAttribute("disabled");
+      this.characterSelection1.value = 0;
+      this.characterSelection2.value = 0;
+      this.characterSelection1.removeAttribute("disabled");
+    });
 
-UI.prototype.initialize = function () {
-  this.playButton.addEventListener("click", this.startGame);
-  this.exitButton.addEventListener("click", this.exitGame);
-  this.characterSelection1.addEventListener(
-    "change",
-    this.characterSelectionChange
-  );
+    this.characterSelection1.addEventListener("change", (event) => {
+      if (event.target.value !== 0) {
+        while (this.characterSelection2.lastChild) {
+          this.characterSelection2.removeChild(
+            this.characterSelection2.lastChild
+          );
+        }
+        const option = document.createElement("option");
 
-  this.setControls(true);
-  this.characterSelection2.setAttribute("disabled", true);
-  this.loadCharacters();
-};
+        option.setAttribute("value", "0");
+        option.setAttribute("disabled", true);
+        option.textContent = "Seleccionar Personaje 2";
 
-UI.prototype.loadCharacters = function () {
-  const that = this;
-  try {
-    fetch("./data/characters.json")
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        for (const character of data) {
+        this.characterSelection2.appendChild(option);
+
+        const filteredCharacters = this.characters.filter(
+          (character) => character.id !== event.target.value
+        );
+
+        for (const character of filteredCharacters) {
           const option = document.createElement("option");
 
           option.setAttribute("value", character.id);
           option.textContent = character.name;
 
-          that.characterSelection1.appendChild(option);
-          that.characters.push(character);
+          this.characterSelection2.appendChild(option);
         }
-      });
-  } catch (error) {
-    log.error(error);
-  }
-};
+        this.characterSelection2.removeAttribute("disabled");
+      }
+    });
 
-UI.prototype.characterSelectionChange = function (event) {
-  if (event.target.value !== 0) {
-    while (this.characterSelection2.lastChild) {
-      this.characterSelection2.removeChild(this.characterSelection2.lastChild);
+    this.characterSelection2.setAttribute("disabled", true);
+    this.setControls(true);
+    this.loadCharacters(this);
+  },
+
+  setControls: function (value) {
+    const controlBtns = [
+      this.attackButton,
+      this.healButton,
+      this.switchButton,
+      this.exitButton,
+    ];
+
+    for (const btn of controlBtns) {
+      if (value) btn.setAttribute("disabled", value);
+      else btn.removeAttribute("disabled");
     }
-    const option = document.createElement("option");
+  },
 
-    option.setAttribute("value", "0");
-    option.setAttribute("disabled", true);
-    option.textContent = "Seleccionar Personaje 2";
+  loadCharacters: function (context) {
+    try {
+      fetch("./data/characters.json")
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          for (const character of data) {
+            const option = document.createElement("option");
 
-    this.characterSelection2.appendChild(option);
+            option.setAttribute("value", character.id);
+            option.textContent = character.name;
 
-    const filteredCharacters = this.characters.filter(
-      (character) => character.id !== event.target.value
-    );
-
-    for (const character of filteredCharacters) {
+            context.characterSelection1.appendChild(option);
+            context.characters.push(character);
+          }
+        });
+    } catch (error) {
+      console.error(error);
       const option = document.createElement("option");
 
-      option.setAttribute("value", character.id);
-      option.textContent = character.name;
+      option.setAttribute("value", "1");
+      option.textContent = "No se encontraron personajes";
 
-      this.characterSelection2.appendChild(option);
+      context.characterSelection1.appendChild(option);
     }
-    this.characterSelection2.removeAttribute("disabled");
-  }
-};
-
-UI.prototype.startGame = function () {
-  if (
-    this.characterSelection1.value === "0" ||
-    this.characterSelection2.value === "0"
-  )
-    return alert("Debes seleccionar ambos personajes para jugar.");
-
-  this.setControls(false);
-  this.playButton.setAttribute("disabled", true);
-  this.characterSelection1.setAttribute("disabled", true);
-  this.characterSelection2.setAttribute("disabled", true);
-};
-
-UI.prototype.exitGame = function () {
-  this.setControls(true);
-  this.playButton.removeAttribute("disabled");
-  this.characterSelection1.value = 0;
-  this.characterSelection2.value = 0;
-  this.characterSelection1.removeAttribute("disabled");
+  },
 };
 
 export default UI;
